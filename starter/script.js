d858f9f80715210677738bc8e7678362a1ea54f2
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-06-23 11:24:42 +0100

    save button generates data and displays correctly

--- starter/script.js
+++ starter/script.js
@@ -1,15 +1,19 @@
-//disables search button if input field empty
+var apiKey = "76dd56a7c869514402bbcfd7dbd7cbb7";
+var cityHistory = [];
+
+// Display Weather Dashboard Title
+$("#dashboard-title").text("Weather Dashboard");
+
+// Disable search button if input field is empty
 $("#search-input").on("input change", function () {
-  if ($(this).val() != "") {
+  if ($(this).val() !== "") {
     $("#search-button").prop("disabled", false);
   } else {
     $("#search-button").prop("disabled", true);
   }
 });
 
-var cityHistory = JSON.parse(localStorage.getItem("City")) || [];
-
-// Local Storage - Saves Destinations
+// Local Storage - Save Destinations
 function saveDestinations() {
   var city = $("#search-input").val().trim();
   if (cityHistory.includes(city)) {
@@ -18,176 +22,136 @@ function saveDestinations() {
   cityHistory.push(city);
   localStorage.setItem("City", JSON.stringify(cityHistory));
 }
-//End Local Storage
 
-//Search button function
+// Display Saved Cities
+function displaySavedCities() {
+  $("#history").empty();
+  cityHistory.forEach(function (city) {
+    var savedCity = $("<button class='savedCity'></button>");
+    savedCity.text(city);
+    savedCity.on("click", function () {
+      var city = $(this).text();
+      displayWeather(city);
+    });
+    $("#history").append(savedCity);
+  });
+}
+
+// Display Weather Data
+function displayWeather(city) {
+  currentWeather(city);
+  forecast(city);
+}
+
+// Search button function
 $("#search-button").on("click", function (e) {
   var city = $("#search-input").val().trim();
-  e.preventDefault();
-  e.stopPropagation();
-  currentWeather();
-  saveDestinations();
-  //create buttons with saved data
-  var searched = $("<button class = savedCity></button>");
-  searched.text(city);
-  $(searched).on("click", function () {
-    currentWeather();
-    forecast();
-  });
-  $("#history").append(searched);
-  // console.log(searched);
-  // console.log(city);
-
-  var apiKey = "76dd56a7c869514402bbcfd7dbd7cbb7";
-  $("#today").empty();
-  $("#forecast").empty();
-  currentWeather();
-  forecast();
-
-  //current weather call
-  function currentWeather() {
-    // Clear out the value in the input field
+  if (city !== "") {
+    e.preventDefault();
+    e.stopPropagation();
+    saveDestinations();
+    displayWeather(city);
     $("#search-input").val("");
-
-    $.ajax({
-      url:
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        apiKey,
-      method: "GET",
-    }).then(function (response) {
-      if (city === "") {
-        //not working
-        alert("Please try again");
-      } else {
-        $(".current").empty(); //partially empties?
-        $(".forecast").empty();
-        // console.log(response);
-        // console.log(city);
-
-        // Creating a div to hold the weather
-        var currentDiv = $("<div class='current'>");
-
-        // Storing the name of the city
-        var cityName = response.name;
-
-        // console.log("The city is called: " + cityName);
-
-        //Storing the date
-        var date = response.dt;
-        var dateString = moment.unix(date).format("DD/MM/YYYY");
-        var pOne = $("<h2>").text(cityName + "(" + dateString + ")");
-        // console.log(dateString);
-        // console.log(date)
-
-        //Storing the image icon
-        var iconCode = response.weather[0].icon;
-        var iconURL = $("<img>").attr(
-          "src",
-          "https://openweathermap.org/img/wn/" + iconCode + ".png"
-        );
-        // console.log(iconCode);
-        var image = iconURL;
-
-        // Storing the temp
-        var temp = response.main.temp;
-        // console.log("Temp: " + temp);
-        var pTwo = $("<p>").text("Temp: " + temp + "°C");
-
-        // Storing the wind speed
-        var windSpeed = response.wind.speed;
-        // console.log("Wind Speed: " + windSpeed);
-        var pFour = $("<p>").text("Wind Speed: " + windSpeed + "mph");
-
-        // Storing the humidity
-        var humidity = response.main.humidity;
-        // console.log("Humidity: " + humidity);
-        var pThree = $("<p>").text("Humidty: " + humidity + "%");
-
-        currentDiv
-          .append(pOne)
-          .append(image)
-          .append(pTwo)
-          .append(pThree)
-          .append(pFour);
-
-        $("#today").append(currentDiv);
-      }
-    });
-  }
-
-  // 5 day forecast call
-  function forecast() {
-    var title = $("<h3>").text("5 Day Forecast:");
-    var forecast =
-      "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      city +
-      "&units=metric&appid=" +
-      apiKey;
-
-    $.ajax({
-      url: forecast,
-      method: "GET",
-    }).then(function (response) {
-      // console.log(response);
-    
-
-      //create a variable to store response
-      var weatherArray = response.list;
-      var container = $("<div class='container'>");
-      //loop through the returned array
-      for (var i = 0; i < weatherArray.length; i++) {
-        //create a div to hold the forecast in
-        var forecastDiv = $("<div class='forecast'>");
-        //grab the information each day at the time of 12:00:00
-        if (weatherArray[i].dt_txt.split(" ")[1] === "12:00:00") {
-          //get the date from each day in the forecast
-          var date = moment(response.list[i].dt_txt.split(" ")[0]).format(
-            "DD-MM-YYYY"
-          );
-          // console.log("Date: " + date);
-          var pdate = $("<p>").text(date);
-
-          var iconCode = response.list[i].weather[0].icon;
-          var iconURL = $("<img>").attr(
-            "src",
-            "https://openweathermap.org/img/wn/" + iconCode + ".png"
-          );
-          // console.log(iconCode);
-          var image = iconURL;
-
-          // Retrieving and storing the temp
-          var temp = response.list[i].main.temp;
-          // console.log("Temp: " + temp + "°C");
-          var ptemp = $("<p>").text("Temp: " + temp + "°C");
-
-          //Retrieving and storing the humidity
-          var humidity = response.list[i].main.humidity;
-          // console.log("Humidty: " + humidity + "%");
-          var phumid = $("<p>").text("Humidity: " + humidity + "%");
-
-          //Retrieving and storing the wind
-          var wind = response.list[i].wind.speed;
-          // console.log("Wind Speed: " + wind + "mph");
-          var pwind = $("<p>").text("Wind Speed: " + wind + "mph");
-
-          //Adding data to forecastDiv
-          forecastDiv
-            .append(pdate)
-            .append(iconURL)
-            .append(ptemp)
-            .append(phumid)
-            .append(pwind);
-
-          //Adding data to forecast
-
-          $("#forecast").append(forecastDiv);
-          $("#forecast").append(container);
-          $("#forecast").append(title);
-          $("#forecast-title").append(title);
-        }
-      }
-    });
+    displaySavedCities(); // Update saved cities display
   }
 });
+
+// Enter key press event handler for search input
+$("#search-input").on("keypress", function (e) {
+  if (e.key === "Enter") {
+    var city = $(this).val().trim();
+    if (city !== "") {
+      e.preventDefault();
+      saveDestinations();
+      displayWeather(city);
+      $(this).val("");
+      displaySavedCities(); // Update saved cities display
+    }
+  }
+});
+
+// Current weather call
+function currentWeather(city) {
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey,
+    method: "GET",
+  }).then(function (response) {
+    var currentDiv = $("<div class='current'>");
+    var cityName = response.name;
+    var date = response.dt;
+    var dateString = moment.unix(date).format("DD/MM/YYYY");
+    var pOne = $("<h2>").text(cityName + " (" + dateString + ")");
+    var iconCode = response.weather[0].icon;
+    var iconURL = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + iconCode + ".png");
+    var image = iconURL;
+    var temp = response.main.temp;
+    var pTwo = $("<p>").text("Temp: " + temp + "°C");
+    var windSpeed = response.wind.speed;
+    var pFour = $("<p>").text("Wind Speed: " + windSpeed + "mph");
+    var humidity = response.main.humidity;
+    var pThree = $("<p>").text("Humidity: " + humidity + "%");
+
+    currentDiv
+      .append(pOne)
+      .append(image)
+      .append(pTwo)
+      .append(pThree)
+      .append(pFour);
+
+    $("#today").empty().append(currentDiv);
+  });
+}
+
+// 5 day forecast call
+function forecast(city) {
+  var forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=" + apiKey;
+  var title = $("<h3>").text("5 Day Forecast:");
+  $.ajax({
+    url: forecast,
+    method: "GET",
+  }).then(function (response) {
+    var weatherArray = response.list;
+    var container = $("<div class='container'>");
+
+    $("#forecast").empty().append(title).append(container);
+
+    for (var i = 0; i < weatherArray.length; i++) {
+      var forecastDiv = $("<div class='forecast'>");
+
+      if (weatherArray[i].dt_txt.split(" ")[1] === "12:00:00") {
+        var date = moment(response.list[i].dt_txt.split(" ")[0]).format("DD-MM-YYYY");
+        var pdate = $("<p>").text(date);
+        var iconCode = response.list[i].weather[0].icon;
+        var iconURL = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + iconCode + ".png");
+        var image = iconURL;
+        var temp = response.list[i].main.temp;
+        var ptemp = $("<p>").text("Temp: " + temp + "°C");
+        var humidity = response.list[i].main.humidity;
+        var phumid = $("<p>").text("Humidity: " + humidity + "%");
+        var wind = response.list[i].wind.speed;
+        var pwind = $("<p>").text("Wind Speed: " + wind + "mph");
+
+        forecastDiv
+          .append(pdate)
+          .append(iconURL)
+          .append(ptemp)
+          .append(phumid)
+          .append(pwind);
+
+        $("#forecast").append(forecastDiv);
+      }
+    }
+  });
+}
+
+// On page load, retrieve saved cities from local storage (if any) and display them
+$(document).ready(function () {
+  cityHistory = JSON.parse(localStorage.getItem("City")) || [];
+});
+
+// On page refresh, remove saved cities from local storage
+$(window).on("beforeunload", function () {
+  localStorage.removeItem("City");
+});
+
+
